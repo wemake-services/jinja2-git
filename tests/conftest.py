82f5c7405d5bbdbc332f32d0b84d93ec68f79088
Nikita Sobolev <mail@sobolevn.me>
2022-07-18 15:51:37 +0300

    Version 1.2.0 release (#147)
    * Version 1.2.0 release
    
    * Version 1.2.0 release

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from jinja2 import Environment
 
 
 @pytest.fixture()
-def environment():
+def environment() -> Environment:
     return Environment(
         extensions=['jinja2_git.GitExtension'],
         autoescape=True,
--- tests/test_gitcommit.py
+++ tests/test_gitcommit.py
@@ -1,22 +1,24 @@
 import subprocess
 
+from jinja2 import Environment
+
 _SHORT_COMMIT_LENGTH = 7
 _FULL_COMMIT_LENGTH = 40
 
 
-def test_commit_length(environment):
+def test_commit_length(environment: Environment) -> None:
     template = environment.from_string('{% gitcommit %}')
 
     assert len(template.render()) == _FULL_COMMIT_LENGTH
 
 
-def test_short_commit_length(environment):
+def test_short_commit_length(environment: Environment) -> None:
     template = environment.from_string('{% gitcommit short=True %}')
 
     assert len(template.render()) == _SHORT_COMMIT_LENGTH
 
 
-def test_is_commit(environment):
+def test_is_commit(environment: Environment) -> None:
     template = environment.from_string('{% gitcommit %}')
     output = subprocess.check_output(
         ['git', 'cat-file', '-t', template.render()],
