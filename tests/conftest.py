822cf5b13c88511dfde4cd323402b603616bca0d
sobolevn <mail@sobolevn.me>
2024-06-27 17:31:07 +0300

    Fix CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from jinja2 import Environment
 
 
-@pytest.fixture()
+@pytest.fixture
 def environment() -> Environment:
     return Environment(
         extensions=['jinja2_git.GitExtension'],
